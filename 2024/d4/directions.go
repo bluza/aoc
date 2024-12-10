1912062413f3113d2fe0d696235340dc078379d3
Fabian Bernegger <fabian.bernegger@kontron.com>
2024-12-10 13:04:33 +0100

    tidy up - still not working

--- 2024/d4/directions.go
+++ 2024/d4/directions.go
@@ -5,25 +5,6 @@ type Direction struct {
 	y int
 }
 
-func (d Direction) Next(increment int) (int, int) {
-	var x, y int
-	if d.x == 0 {
-		x = 0
-	} else if d.x == 1 {
-		x = increment
-	} else {
-		x = -increment
-	}
-	if d.y == 0 {
-		y = 0
-	} else if d.y == 1 {
-		y = increment
-	} else {
-		y = -increment
-	}
-	return x, y
-}
-
 var Directions = []Direction{
 	{0, 1},
 	{0, -1},
--- 2024/d4/main.go
+++ 2024/d4/main.go
@@ -7,34 +7,6 @@ import (
 	"os"
 )
 
-func FindString(row int, col int, dir Direction, field *[][]byte) bool {
-
-	tofind := "XMAS"
-	tfield := *field
-	if tfield[row][col] != byte(tofind[0]) {
-		return false
-	}
-	for i, c := range tofind[1:] {
-		x, y := dir.Next(i + 1)
-		//fmt.Println("move to: ", x, y)
-		nxt_row := row + x
-		nxt_col := col + y
-		if nxt_col < 0 || nxt_col > len(tfield[0])-1 {
-			return false
-		}
-		if nxt_row < 0 || nxt_row > len(tfield)-1 {
-			return false
-		}
-		//fmt.Println(string(tfield[next_row][new_col]))
-		if tfield[nxt_row][nxt_col] != byte(c) {
-			return false
-		}
-	}
-	fmt.Println("found xmas: ", row, col, dir)
-	return true
-
-}
-
 func readFile(path string) [][]byte {
 
 	file, err := os.Open(path)
@@ -44,7 +16,6 @@ func readFile(path string) [][]byte {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-
 	var field [][]byte
 	for scanner.Scan() {
 		field = append(field, scanner.Bytes())
@@ -52,12 +23,44 @@ func readFile(path string) [][]byte {
 	return field
 }
 
-func main() {
+func outofbounds(row int, col int, side_length int) bool {
+	if col < 0 || col > side_length-1 {
+		return true
+	}
+	if row < 0 || row > side_length-1 {
+		return true
+	}
+	return false
+}
 
-	field := readFile("./test.txt")
+func FindString(row int, col int, dir Direction, field *[][]byte) bool {
+
+	tofind := "XMAS"
+	tfield := *field
+	if tfield[row][col] != byte(tofind[0]) {
+		return false
+	}
+	current_row := row
+	current_col := col
+	for _, c := range tofind[1:] {
+		current_row += dir.x
+		current_col += dir.y
+		if outofbounds(current_row, current_col, len(tfield)) {
+			return false
+		}
+		if tfield[current_row][current_col] != byte(c) {
+			return false
+		}
+	}
+	fmt.Println("found xmas: ", row, col, dir)
+	return true
+}
+
+func main() {
+	field := readFile("input.txt")
 	counter := 0
-	for row := 0; row < len(field); row++ {
-		for col := 0; col < len(field[0]); col++ {
+	for row := range field {
+		for col := range field[0] {
 			for _, d := range Directions {
 				if FindString(row, col, d, &field) {
 					counter++
